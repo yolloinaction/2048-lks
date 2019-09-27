c5a2456dd95328232ce897df08c350f4384d5177
yuandahanif <kirishima699@gmail.com>
2019-09-27 16:01:31 +0700

    memperbaiki random add number

--- from codingtrain/2048/sketch.js
+++ from codingtrain/2048/sketch.js
@@ -13,8 +13,6 @@ function setup() {
   grid = addNumber(grid)
   grid = addNumber(grid)
 
-  // noLoop()
-
 }
 
 function draw() {
@@ -23,37 +21,20 @@ function draw() {
   background(255)
   stroke(1)
   // draw the playground
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-      noFill()
-      rect(i*(width/4),j*(height/4),width/4,height/4)
-      displayNumb(grid);
-    }
-  }
-  
+  drawGrid()
+  displayNumb(grid);
 }
 
 
 // *function goes here
-
-// ? add number to board
-function addNumber(grid) {
-  let option = {x:0,y:0}
-
-
-  for (let i = 0; i < 16; i++) {
-    let x = round(random(3))
-    let y = round(random(3))
-
-      if (grid[x][y] == 0 ) {
-        option.x = x
-        option.y = y
-        break
-      }
+// ?draw the grid
+function drawGrid() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      noFill()
+      rect(i*(width/4),j*(height/4),width/4,height/4)
+    }
   }
-  
-  grid[option.x][option.y] = random(1) > 0.5 ? 4 : 2 ;
-  return grid
 }
 
 // ? display number to the board
@@ -67,6 +48,38 @@ function displayNumb(grid){
       text(grid[j][i] ,i*(width/4)+50,j*(width/4)+50)
     }
   }
+}
+
+// ? add number to board
+function addNumber(grid) {
+  let option = {x:0,y:0}
+  let arrTmp = []
+  for (let i = 0; i < grid.length; i++) {
+    console.log(grid[i].length);
+    
+      if (grid[i].length == 0) {
+        console.log("sip");
+      }
+      
+      arrTmp.push(grid[i].filter( v => !v));
+  }
+  console.table(arrTmp.filter( v => v !== []));
+  // if (arrTmp.fil) {
+    
+  // }
+  
+  while (true){
+    let x = round(random(3))
+    let y = round(random(3))
+
+      if (grid[x][y] == 0 ) {
+        option.x = x
+        option.y = y
+        break
+      }
+  }
+  grid[option.x][option.y] = random(1) > 0.5 ? 4 : 2 ;
+  return grid
 }
 
 // ? geser angka nya
