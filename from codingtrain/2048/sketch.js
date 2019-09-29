fcf8979eaa365c27fe41c54090e892aa925f9a82
yuandahanif <Kirishima699@gmail.com>
2019-09-29 14:06:20 +0700

    fungsi gerak atas,bawah,kiri,dan kanan OK fungsi penambahan OK otw fungsi game over

--- from codingtrain/2048/sketch.js
+++ from codingtrain/2048/sketch.js
@@ -53,50 +53,229 @@ function displayNumb(grid){
 // ? add number to board
 function addNumber(grid) {
   let option = {x:0,y:0}
-  let arrTmp = []
-  for (let i = 0; i < grid.length; i++) {
-    console.log(grid[i].length);
-    
-      if (grid[i].length == 0) {
-        console.log("sip");
-      }
-      
-      arrTmp.push(grid[i].filter( v => !v));
+  let gridTmp = []
+  // * lihat dan tampung jumlah column dari grid yang masih kosong
+  for (let x = 0; x < grid.length; x++) {
+      for (let y = 0; y < grid[x].length; y++) {
+        if (grid[x][y] == 0) {
+          gridTmp.push({x,y})
+        }
+    }
   }
-  console.table(arrTmp.filter( v => v !== []));
-  // if (arrTmp.fil) {
-    
-  // }
-  
-  while (true){
-    let x = round(random(3))
-    let y = round(random(3))
-
-      if (grid[x][y] == 0 ) {
-        option.x = x
-        option.y = y
-        break
-      }
+  if (gridTmp.length != 0) {
+    let tmp = random(gridTmp)
+    option.x = tmp.x
+    option.y = tmp.y
+    grid[option.x][option.y] = random(1) > 0.5 ? 4 : 2 ;
+    return grid
   }
-  grid[option.x][option.y] = random(1) > 0.5 ? 4 : 2 ;
   return grid
 }
 
-// ? geser angka nya
-function doSlide(row) {
-  let arr  = row.filter( v => v)
-  let zeros = Array( 4 - arr.length ).fill(0)
-  return arr = zeros.concat(arr)
+// ? sum same number
+function sumNumber(grid,direction) {
+
+switch (direction) {
+  case 'right':
+    for (let i = 3; i >= 0; i--) {
+      for (let j = 3; j >= 0; j--) {
+        // * cek apakah sama
+        if (grid[i][j] == grid[i][j-1]) {
+          grid[i][j] = grid[i][j] + grid[i][j-1]
+          grid[i][j-1] = 0
+        }
+      }
+    }
+    break;
+
+  case 'left':
+    for (let i = 0; i <= 3; i++) {
+      for (let j = 0; j <= 3; j++) {
+        // * cek apakah sama
+        if (grid[i][j] == grid[i][j+1]) {
+          grid[i][j] = grid[i][j] + grid[i][j+1]
+          grid[i][j+1] = 0
+        }
+      }
+    }
+    break;
+
+  case 'down':
+        for (let i = 3; i >= 0; i--) { //index lv.1
+          for (let j = 3; j >= 0; j--) { //index lv.2
+            // * cek apakah sama
+            if (j != 0 && grid[j][i] == grid[j-1][i]) {
+              grid[j][i] = grid[j][i] + grid[j-1][i]
+              grid[j-1][i] = 0
+            }
+          }
+        }
+    break;
+
+  case 'up':
+        for (let i = 0; i <= 3; i++) { //index lv.1
+          for (let j = 0; j <= 3; j++) { //index lv.2
+            // * cek apakah sama
+            if (j != 3 && grid[j][i] == grid[j+1][i]) {
+              console.log('lol')
+              grid[j][i] = grid[j][i] + grid[j+1][i]
+              grid[j+1][i] = 0
+            }
+          }
+        }
+      break;
+
+  default:
+    alert('error : the second parameter must be horizon | up | down')
+    break;
+}
 }
 
+
+// ? geser angka nya
+function doSlide(row,direction = 'left') {
+  let arr  = row.filter( v => v)
+  let zeros = Array( 4 - arr.length ).fill(0)
+  return direction == 'left' ? arr = zeros.concat(arr) : arr = arr.concat(zeros)
+}
+
+
+// TODO: key pressed
 // ? when key is pressed
 function keyPressed() {
+  let gridClone = [...grid]
 
-  if (keyCode == LEFT_ARROW ) {
+  // * arrow kanan
+  if (keyCode == RIGHT_ARROW ) {
+    for (let i = 0; i < 4; i++) {
+      grid[i] = doSlide(grid[i])
+    }
+    sumNumber(grid,'right')
     for (let i = 0; i < 4; i++) {
       grid[i] = doSlide(grid[i])
     }
     addNumber(grid)
   }
+
+  // * arrow kiri
+  if (keyCode == LEFT_ARROW ) {
+    for (let i = 0; i < 4; i++) {
+      grid[i] = doSlide(grid[i],'kanan')
+    }
+    sumNumber(grid,'left')
+    for (let i = 0; i < 4; i++) {
+      grid[i] = doSlide(grid[i],'!kiri')
+    }
+    addNumber(grid)
+  }
+
+  // * arrow bawah
+  if (keyCode == DOWN_ARROW ) {
+    let rowTmp = []
+    let colTmp = []
+    let gridTmp = []
+
+    // * rotate the grid array
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        colTmp.push(grid[j][i])
+      }
+      rowTmp.push(colTmp)
+      colTmp = []
+      // * lets slide the rotated array
+      gridTmp.push(doSlide(rowTmp[i]))
+    }
+
+    // * restore the rotated array and change the display
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        grid[j][i] = gridTmp[i][j]
+      }
+    }
+
+    sumNumber(grid,'down')
+
+    rowTmp = []
+    colTmp = []
+    gridTmp = []
+
+    // * rotate the grid array
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        colTmp.push(grid[j][i])
+      }
+      rowTmp.push(colTmp)
+      colTmp = []
+      // * lets slide the rotated array
+      gridTmp.push(doSlide(rowTmp[i]))
+    }
+
+    // * restore the rotated array and change the display
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        grid[j][i] = gridTmp[i][j]
+      }
+    }
+
+    addNumber(grid)
+  }
+
+  // * arrow atas
+  if (keyCode == UP_ARROW) {
+
+    let rowTmp = []
+    let colTmp = []
+    let gridTmp = []
+
+    // * rotate the grid array
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        colTmp.push(grid[j][i])
+      }
+      rowTmp.push(colTmp)
+      colTmp = []
+
+      // * lets slide the rotated array
+      gridTmp.push(doSlide(rowTmp[i],'right'))
+    }
+    // * restore the rotated array and change the display
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        grid[j][i] = gridTmp[i][j]
+      }
+    }
+
+    sumNumber(grid,'up')
+
+    rowTmp = []
+    colTmp = []
+    gridTmp = []
+
+    // * rotate the grid array
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        colTmp.push(grid[j][i])
+      }
+      rowTmp.push(colTmp)
+      colTmp = []
+
+      // * lets slide the rotated array
+      gridTmp.push(doSlide(rowTmp[i],'right'))
+    }
+    // * restore the rotated array and change the display
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        grid[j][i] = gridTmp[i][j]
+      }
+    }
+    addNumber(grid)
+  }
+
+  console.log(gridClone);
+  console.log(grid);
+  
+  
+
+
   // return false
 }
